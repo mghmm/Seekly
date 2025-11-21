2ff5f2d814f27470d8d8e31b0e377cb632523e85
Mike Merkelbag <m.merkelbag@hotmail.com>
2025-11-21 13:51:59 +1100

    Initial Commit

---
+++ Seekly/SeeklyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeeklyApp.swift
+//  Seekly
+//
+//  Created by Mike Merkelbag on 21/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SeeklyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
